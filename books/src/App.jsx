3afdf73fd66627a4fb2b847689a610fda1fd6c2e
OOla7 <144017561+OOla7@users.noreply.github.com>
2024-10-01 14:24:38 +0200

    opdrachten

---
+++ books/src/App.jsx
@@ -0,0 +1,20 @@
+import Book from "./componants/Book"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Book image="afbeelding 1" title="intresting facts" 
+      author="Jordan Moore" />
+       <Book image="afbeelding 2" title="Atomic habits" 
+      author="James Clear" />
+       <Book image="afbeelding 3" title="Fairy tale" 
+      author="Stephan King" />
+
+    </>
+  )
+}
+
+export default App
